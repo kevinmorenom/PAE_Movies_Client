0440eae4e4f510e3aa09fa3f7ca0fddde37563e4
Juanp44 <is715592@iteso.mx>
2020-11-24 22:58:44 -0600

    Update: Began multer implementation and fixed style on profile

--- src/app/global/components/profile-info/profile-info.component.ts
+++ src/app/global/components/profile-info/profile-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-info',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-info.component.scss']
 })
 export class ProfileInfoComponent implements OnInit {
+  form: FormGroup;
+  file: File;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      image: ['', Validators.required]
+    })
+  }
+
+  onFileChange(){
+    // this.file = <File>event.target.file[0];
+    
+    if(!this.form){return;}
+    const values = this.form.getRawValue();
+    console.log(values);
   }
 
 }
