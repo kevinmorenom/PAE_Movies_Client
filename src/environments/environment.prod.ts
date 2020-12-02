45bf0d46665a75ad9b157f7405e8e38a2f5e6132
Juanp44 <is715592@iteso.mx>
2020-12-01 19:34:03 -0600

    Final style changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:3001',
+  clientId: '621493509847-7rfirgkcs99hhrqluv9rlu8b745g86ns.apps.googleusercontent.com',
+  socketUrl: 'http://localhost:3001'
 };
